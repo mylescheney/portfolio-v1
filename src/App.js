5162e9dfbb3950458dffcc90e8dc5fa377ecfb96
Myles <mylescheney@gmail.com>
2022-06-20 17:23:44 -0400

    Set up the area I plan to insert the banner image

--- src/App.js
+++ src/App.js
@@ -6,12 +6,21 @@ import NavBarButton from './components/NavBarButton';
 
 function App() {
   return (
-    <div className='navBar'>
-      <NavBarButton title='Greetings' />
-      <NavBarButton title='About Me' />
-      <NavBarButton title='Projects' />
-      <NavBarButton title='Contact' />
-    </div>
+    <div>
+      <div className='navBar'>
+        <NavBarButton title='Greetings' />
+        <NavBarButton title='About Me' />
+        <NavBarButton title='Projects' />
+        <NavBarButton title='Contact' />
+      </div>
+      <div className='periBanner'>
+        <div className='banner'>
+          <div className='bannerImage'>
+            <p style={{ color: '#fff' }}>Banner image goes here</p>
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
 
